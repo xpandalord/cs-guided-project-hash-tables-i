9ba99ba2ada037fdd0f923a258d897dfdc619df7
Andy Tran <xpandalord@gmail.com>
2020-10-20 12:04:49 -0400

    updates

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,17 +1,17 @@
 """
 Your task is create your own HashTable without using a built-in library.
-
+​
 Your HashTable needs to have the following functions:
-
+​
 - put(key, value) : Inserts a (key, value) pair into the HashTable. If the
 value already exists in the HashTable, update the value.
 - get(key): Returns the value to which the specified key is mapped, or -1 if
 this map contains no mapping for the key.
 - remove(key) : Remove the mapping for the value key if this map contains the
 mapping for the key.
-
+​
 Example:
-
+​
 ```plaintext
 hash_table = MyHashTable();
 hash_table.put("a", 1);
@@ -24,25 +24,79 @@ hash_table.remove("b");         // remove the mapping for 2
 hash_table.get("b");            // returns -1 (not found)
 ```
 """
-class ListNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-
 class MyHashTable:
-    def __init__(self):
+    def __init__(self, capacity=10):
         # Your code here
-
-
+        # hash tables use an array for storage 
+        self.storage = [None] * capacity
+        # self.storage = [None for _ in range(capacity)]
+        self.num_items = 0
+        self.capacity = capacity
+​
+    # O(k) where k is the size of the key 
+    def _hash(self, key):
+        """
+        DJB2 hash, 32-bit
+        """
+        # Cast the key to a string and get bytes
+        str_key = str(key).encode()
+​
+        # Start from an arbitrary large prime
+        hash_value = 5381
+​
+        # Bit-shift and sum value for each character
+        for b in str_key:
+            hash_value = ((hash_value << 5) + hash_value) + b
+            hash_value &= 0xffffffff  # DJB2 is a 32-bit hash, only keep 32 bits
+​
+        # we need to ensure that this output is actually in bounds of our array 
+        print("hash value before %: ", hash_value)
+​
+        return hash_value % self.capacity
+​
     def put(self, key, value):
         # Your code here
-
-
+        # 1. run our key through our hash function to get the index 
+        index = self._hash(key) # O(k)
+        print("Put index: ", index)
+        # 2. set the key and value in the array at that index 
+        self.storage[index] = (key, value) # O(1)
+        print(self.storage)
+​
     def get(self, key):
         # Your code here
-
-
-    def remove(self, key: int) -> None:
+        # 1. run our key through our hash function to get the index 
+        index = self._hash(key) # O(k)
+        print("Get index: ", index)
+        # 2. access the key-value pair at the index in our array and return 
+        # just the value, which is the second element of the key-value tuple
+        print(self.storage)
+​
+        # handle the case where the key-value pair is not in our hash table 
+        if self.storage[index] is None:
+            return None
+​
+        return self.storage[index][1] # O(1)
+​
+    def remove(self, key):
         # Your code here
-
+        # 1. run our key through our hash function to get the index 
+        index = self._hash(key) # O(k)
+        print("Get index: ", index)
+        # 2. access the key-value pair at the index in our array 
+        # set that spot to None 
+        print(self.storage)
+        self.storage[index] = None # O(1)
+​
+​
+ht = MyHashTable()
+print(ht.storage)
+​
+ht.put("a", "b")
+ht.put("cow", "llama")
+​
+ht.put("cat", "dog")
+ht.put("fish", "bird")
+print(ht.get("cat"))
+print(ht.get("cow"))
+ht.put("i", "j")
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -46,13 +46,31 @@ Notes:
 - All characters in words[i] and order are English lowercase letters.
 """
 def are_words_sorted(words, alpha_order):
-    """
-    Inputs:
-    words: List[str]
-    alpha_order: str
-
-    Output:
-    bool
-    """
-    # Your code here
+  alpha_map = {char: ind for ind, char in enumerate(alpha_order)}
+  # keep track of whether we've come across any words out of order
+  flag = True
+  for index in range(1, len(words)):
+    if flag == True:
+      flag = compareWords(words[index - 1], words[index], alpha_map)
+  return flag
+def compareWords(word1, word2, char_map):
+  least_length = min(len(word1), len(word2))
+  print(f'{word1}, {word2}')
+  if len(word1) < len(word2) and word1 == word2[:(len(word1))]:
+    return True
+  if len(word1) > len(word2) and word2 == word1[:(len(word2))]:
+    return False
+  for ind, char in enumerate(word1[:least_length]):
+    # if the chars are in correct alphabetical order
+    # return True
+    if char_map[word1[ind]] < char_map[word2[ind]]:
+      return True
+    # if they are in incorrect alphabetical order
+    # return False
+    if char_map[word1[ind]] > char_map[word2[ind]]:
+      return False 
+  return True
+words = ["abcde", "abcdef", 'abcdefg']
+order = "abcdefghijklmnopqrstuvwxyz"
+print(are_words_sorted(words, order))
 
